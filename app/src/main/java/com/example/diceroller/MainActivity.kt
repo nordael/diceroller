b38cf4a6b42e02321bcf67f8ce3a94ee61edd312
nordael <jleandro.martins@gmail.com>
2021-11-12 16:59:31 -0300

    adding an event onclick on button showing a diferent lable text after click on it.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +10,10 @@ class MainActivity : AppCompatActivity() {
         //the line below shows the content of the layout content described in activity_mani.xml
         //in layout folder under ./res directory
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById<Button>(R.id.roll_button)
+        rollButton.text = "Let's roll it!"
+        rollButton.setOnClickListener{
+            rollButton.text = "Wow, you clicked it!"
+        }
     }
 }
