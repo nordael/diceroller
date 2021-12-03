a223a36798cad67d8380189696c7eefde1b74ffd
nordael <jleandro.martins@gmail.com>
2021-12-03 13:35:57 -0300

    (fix) aula4_14: using the lifecycle observer to set timer properly

--- aula4/app/src/main/java/com/example/android/dessertpusher/DessertTimer.kt
+++ aula4/app/src/main/java/com/example/android/dessertpusher/DessertTimer.kt
@@ -18,6 +18,9 @@ package com.example.android.dessertpusher
 
 import android.os.Handler
 import android.os.Looper
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
 import timber.log.Timber
 
 /**
@@ -35,7 +38,7 @@ import timber.log.Timber
  * https://developer.android.com/guide/components/processes-and-threads
  *
  */
-class DessertTimer {
+class DessertTimer( lifecycle : Lifecycle) : LifecycleObserver{
 
     // The number of seconds counted since the timer started
     var secondsCount = 0
@@ -47,7 +50,16 @@ class DessertTimer {
     private var handler = Handler(Looper.getMainLooper())
     private lateinit var runnable: Runnable
 
+    init {
+        lifecycle.addObserver( this )
+    }
 
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    fun dummyMethod(){
+        Timber.i("I was called 'cause onPause has triggered ")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
     fun startTimer() {
         // Create the runnable action, which prints out a log and increments the seconds counter
         runnable = Runnable {
@@ -66,6 +78,7 @@ class DessertTimer {
         // In this case, no looper is defined, and it defaults to the main or UI thread.
     }
 
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
     fun stopTimer() {
         // Removes all pending posts of runnable from the handler's queue, effectively stopping the
         // timer
