3c362e24d9c6275b0133793d68ffc75e6d858d67
nordael <jleandro.martins@gmail.com>
2021-12-07 16:04:40 -0300

    (feat) aula5_22: added score live data to view model

---
+++ aula5/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.android.guesstheword.screens.score
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ScoreViewModel (finalScore: Int ): ViewModel() {
+
+    private var score = MutableLiveData<Int>()
+        val ldScore : LiveData<Int>
+            get() = score
+    private var eventPlayAgain = MutableLiveData<Boolean>()
+        val ldEventPlayAgain : LiveData<Boolean>
+            get() = eventPlayAgain
+
+    init {
+        score.value = finalScore
+        eventPlayAgain.value = false
+        Log.i("ScoreViewModel", "final score $finalScore")
+    }
+
+    fun setEventPLayAgain( bool : Boolean ){
+        eventPlayAgain.value = bool
+    }
+
+    fun getScore() : LiveData<Int> {
+        return ldScore
+    }
+
+    fun getEventPlayAgain() : LiveData<Boolean>{
+        return ldEventPlayAgain
+    }
+
+}
