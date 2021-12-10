a09f57292e0e21c232b06cadeab072979b5510df
nordael <jleandro.martins@gmail.com>
2021-12-10 14:25:05 -0300

    feat(database): created the queries to manipulate data on database

--- aula6/app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
+++ aula6/app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
@@ -24,7 +24,7 @@ import androidx.room.PrimaryKey
 data class SleepNight(
     @PrimaryKey(autoGenerate = true)
     var nightId : Long = 0L,
-    
+
     @ColumnInfo(name = "start_time_milli")
     val startTimeMilli: Long = System.currentTimeMillis(),
 
