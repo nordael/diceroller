107b886c1e67d7b58ecbabd85282b7f1fe40f5a8
nordael <jleandro.martins@gmail.com>
2021-12-14 11:16:05 -0300

    feat(view_model): added co-routines to deal with database conection

--- aula6/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ aula6/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -17,10 +17,7 @@
 package com.example.android.trackmysleepquality.sleeptracker
 
 import android.app.Application
-import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
+import androidx.lifecycle.*
 import com.example.android.trackmysleepquality.database.SleepDatabaseDao
 import com.example.android.trackmysleepquality.database.SleepNight
 import com.example.android.trackmysleepquality.formatNights
@@ -38,7 +35,13 @@ class SleepTrackerViewModel(
         private val nights = database.getAllNights()
 
         private var viewModelJob = Job()
+
         private val uiScope = CoroutineScope(Dispatchers.Main +  viewModelJob)
+
+        private val _navigateToSleepQuality = MutableLiveData<SleepNight>()
+
+        val navigateToSleepQuality: LiveData<SleepNight>
+                get() = _navigateToSleepQuality
 
         val nightsString = Transformations.map(nights) { nights ->
                 formatNights(nights, application.resources)
@@ -64,6 +67,10 @@ class SleepTrackerViewModel(
                 }
         }
 
+        fun doneNavigating() {
+                _navigateToSleepQuality.value = null
+        }
+
         fun onStartTracking() {
                 uiScope.launch {
                         val newNight = SleepNight()
@@ -77,6 +84,7 @@ class SleepTrackerViewModel(
                         val oldNight = tonight.value ?: return@launch
                         oldNight.endTimeMilli = System.currentTimeMillis()
                         update(oldNight)
+                        _navigateToSleepQuality.value = oldNight
                 }
         }
 
