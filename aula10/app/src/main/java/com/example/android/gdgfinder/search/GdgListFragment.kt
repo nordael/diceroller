d5020b172f01ab4339ffbc73b718605301f9a98d
nordael <jleandro.martins@gmail.com>
2022-01-07 14:15:45 -0300

    feat(layout): added chip group to filter search on gdg list fragment

--- aula10/app/src/main/java/com/example/android/gdgfinder/search/GdgListFragment.kt
+++ aula10/app/src/main/java/com/example/android/gdgfinder/search/GdgListFragment.kt
@@ -11,8 +11,10 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.android.gdgfinder.R
 import com.example.android.gdgfinder.databinding.FragmentGdgListBinding
 import com.google.android.gms.location.*
+import com.google.android.material.chip.Chip
 import com.google.android.material.snackbar.Snackbar
 
 private const val LOCATION_PERMISSION_REQUEST = 1
@@ -41,6 +43,7 @@ class GdgListFragment : Fragment() {
             startActivity(Intent(Intent.ACTION_VIEW, destination))
         })
 
+
         // Sets the adapter of the RecyclerView
         binding.gdgChapterList.adapter = adapter
 
@@ -53,6 +56,29 @@ class GdgListFragment : Fragment() {
                         "No location. Enable location in settings (hint: test with Maps) then check app permissions!",
                         Snackbar.LENGTH_LONG
                     ).show()
+                }
+            }
+        })
+
+        viewModel.regionList.observe(viewLifecycleOwner, object: Observer<List<String>> {
+            override fun onChanged(data: List<String>?) {
+                data ?: return
+                val chipGroup = binding.regionsList
+                val inflator = LayoutInflater.from(chipGroup.context)
+
+                val children = data.map { regionName ->
+                    val chip = inflator.inflate(R.layout.region, chipGroup, false) as Chip
+                    chip.text = regionName
+                    chip.tag = regionName
+                    chip.setOnCheckedChangeListener { button, isChecked ->
+                        viewModel.onFilterChanged(button.tag as String, isChecked)
+                    }
+                    chip
+                }
+                chipGroup.removeAllViews()
+
+                for (chip in children) {
+                    chipGroup.addView(chip)
                 }
             }
         })
@@ -134,5 +160,3 @@ class GdgListFragment : Fragment() {
         }
     }
 }
-
-
